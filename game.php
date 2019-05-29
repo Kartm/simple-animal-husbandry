a01588b878f47ed934a7ccb92864c8d5a3169ab6
Kartm <lblachnicki@gmail.com>
2019-05-30 01:52:04 +0200

    Finished exchanging, the game is passing all tests

--- game.php
+++ game.php
@@ -35,8 +35,6 @@
 
             function getRate($from, $to) {
                 $fromArray = $this -> exchangeArray[$from];
-                var_dump($from);
-                var_dump(sizeof($fromArray));
                 for($i = 0; $i < sizeof($fromArray); $i++) {
                     if(strcmp($fromArray[$i] -> to, $to) == 0) {
                         return $fromArray[$i] -> rate;
@@ -84,16 +82,17 @@
             private function removeAnimals($chosenObjectArray, $isRemovingChosen) {
                 $oldAnimals = $this -> animals;
                 if($isRemovingChosen == true) { //* removing only chosen animals
-                    foreach(array_keys($chosenObjectArray) as $chosen) {
+                    foreach(array_values($chosenObjectArray) as $chosen) {
+                        
                         $animalName = $this -> getAnimalName($chosen);
                         unset($this -> animals[$animalName]);
                     }
                 } else { //* removing everything except chosen animals
                     $chosenArray = [];
-
                     foreach(array_keys($chosenObjectArray) as $chosen) {
                         array_push($chosenArray, $this -> getAnimalName($chosenObjectArray[$chosen]));
                     }
+                    
 
                     foreach(array_keys($oldAnimals) as $animalName) { // inefficient, but I'm doing it for the sake of simplicity
                         if(in_array($animalName, $chosenArray) == false) {
@@ -154,12 +153,17 @@
                 $rate = $exchange -> getRate($animal1Name, $animal2Name);
 
                 $animal1Amount = $this -> getAnimalAmount($animal1Object);
+                $animal2Amount = $this -> getAnimalAmount($animal2Object);
 
-                $animal2Amount = floor($animal1Amount * $rate);
-
-                $this -> removeAnimals([$animal1Object], true);
-
-                $this -> addAnimals($animal2Object, $animal2Amount);
+                if($rate > 1) {
+                    $this -> setAnimalAmount($animal1Object, $animal1Amount - 1);
+                    $this -> addAnimals($animal2Object, $rate);
+                } else {
+                    $rate = round(1/$rate);
+                    $this -> setAnimalAmount($animal1Object, $animal1Amount - $rate);
+                    $this -> addAnimals($animal2Object, 1);
+                    
+                }
             }
     
             function attack($animalObject) {
@@ -199,11 +203,105 @@
 
         $herd = new Herd();
         $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Pig, 1);
+        $herd->reproduce(new Animal\Rabbit, new Animal\Pig);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Pig, 1);
+        $herd->reproduce(new Animal\Sheep, new Animal\Pig);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 5);
+        $herd->addAnimals(new Animal\Cow, 1);
+        $herd->reproduce(new Animal\Sheep, new Animal\Pig);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 4);
+        $herd->addAnimals(new Animal\Sheep, 2);
+        $herd->addAnimals(new Animal\Horse, 1);
+        $herd->reproduce(new Animal\Pig, new Animal\Pig);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        //!floating points values?!?!?!?!
+        //* cow - 1
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
         $herd->addAnimals(new Animal\Sheep, 1);
         $herd->addAnimals(new Animal\Pig, 2);
         $herd->exchange(new Animal\Rabbit, new Animal\Sheep);
         $herd->exchange(new Animal\Sheep, new Animal\Pig);
         $herd->exchange(new Animal\Pig, new Animal\Cow);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Horse, 1);
+        $herd->exchange(new Animal\Horse, new Animal\Cow);
+        $herd->exchange(new Animal\Cow, new Animal\Pig);
+        $herd->exchange(new Animal\Pig, new Animal\Sheep);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Sheep, 6);
+        $herd->addAnimals(new Animal\Pig, 6);
+        $herd->addAnimals(new Animal\Cow, 6);
+        $herd->addAnimals(new Animal\Horse, 6);
+        $herd->addAnimals(new Animal\Dog, 1);
+        $herd->attack(new Animal\Wolf);
+        $herd->attack(new Animal\Fox);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Sheep, 6);
+        $herd->addAnimals(new Animal\Pig, 6);
+        $herd->addAnimals(new Animal\Cow, 6);
+        $herd->addAnimals(new Animal\Horse, 6);
+        $herd->addAnimals(new Animal\Dog, 1);
+        $herd->attack(new Animal\Fox);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Sheep, 6);
+        $herd->addAnimals(new Animal\Pig, 6);
+        $herd->addAnimals(new Animal\Cow, 6);
+        $herd->addAnimals(new Animal\Horse, 6);
+        $herd->addAnimals(new Animal\BigDog, 1);
+        $herd->attack(new Animal\Wolf);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Sheep, 6);
+        $herd->addAnimals(new Animal\Pig, 6);
+        $herd->addAnimals(new Animal\Cow, 6);
+        $herd->addAnimals(new Animal\Horse, 6);
+        $herd->attack(new Animal\Fox);
+        var_dump($herd->getAnimals());
+        var_dump("");
+
+        $herd = new Herd();
+        $herd->addAnimals(new Animal\Rabbit, 6);
+        $herd->addAnimals(new Animal\Sheep, 6);
+        $herd->addAnimals(new Animal\Pig, 6);
+        $herd->addAnimals(new Animal\Cow, 6);
+        $herd->addAnimals(new Animal\Horse, 6);
+        $herd->addAnimals(new Animal\Dog, 1);
+        $herd->attack(new Animal\Wolf);
         var_dump($herd->getAnimals());
 
     }
